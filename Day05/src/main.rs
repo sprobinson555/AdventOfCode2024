f9ce4d01e4a5e4bf17823036094fe283dcfd6c20
Scott <sprobinson2@crimson.ua.edu>
2024-12-11 19:42:11 -0600

    input is parsed

--- Day05/src/main.rs
+++ Day05/src/main.rs
@@ -1,3 +1,43 @@
+use std::env;
+use std::fs;
+
+mod grid_traversal;
+use grid_traversal::{
+    Position,
+    Velocity,
+    Unit_Velocity
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("You need to specify a filename input argument")
+    }
+
+    let filepath = &args[1];
+
+    let contents = fs::read_to_string(filepath).expect("Cannot read contents of file");
+
+    let mut rules :Vec<(i32, i32)> = vec![];
+    let mut pages :Vec<Vec<i32>> = vec![]; 
+    for line in contents.lines() {
+        if line.contains("|") {
+            let mut nums = line.split('|');
+            let (num1, num2) = (nums.next().unwrap(), nums.next().unwrap());
+            rules.push((num1.parse::<i32>().unwrap(), num2.parse::<i32>().unwrap()));
+        }
+        if line.contains(",") {
+            let mut page : Vec<i32> = vec![];
+            for nums in line.split(',') {
+                page.push(nums.parse::<i32>().unwrap());
+            }
+            pages.push(page);
+        }
+    }
+
+    println!("rules:\r\n {:?}", rules);
+    println!("pages:\r\n {:?}", pages);
+
 }
