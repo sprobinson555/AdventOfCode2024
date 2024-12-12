0e36d9b527d5f07363ebcb0b6bb6482b72b04f4d
Scott <sprobinson2@crimson.ua.edu>
2024-12-11 23:34:04 -0600

    lists are somewhat sorted but not fully... this one is tough

--- Day05/src/main.rs
+++ Day05/src/main.rs
@@ -43,7 +43,9 @@ fn main() {
     // println!("pages:\r\n {:?}", pages);
 
     let mut sum_of_middles = 0;
-    for page in pages {
+    let pages_copy :Vec<Vec<i32>> = vec![];
+
+    for page in pages.iter() {
         let mut valid = true;
         for (index, num) in page.iter().enumerate() {
             for rule in rules.iter().filter(|(a, b)| a == num) {
@@ -60,6 +62,43 @@ fn main() {
         }
     }
 
-    println!("sum of middles is {sum_of_middles}");
+    println!("sum of middles is {sum_of_middles}\r\n");
 
+    /************************************ PART TWO  **********************************/
+
+    let mut sum_of_middles = 0;
+    for page in pages.iter() {
+        let mut valid = true;
+        let mut new_page :Vec<i32> = vec![];
+        for (index, num) in page.iter().enumerate() {
+            let index_to_insert = page_item_breaks_rules(index, num, &new_page, &rules);
+            if  index_to_insert != new_page.len() {
+                valid = false;
+                new_page.insert(index_to_insert, *num);
+            }
+            else {
+                new_page.push(*num);
+            }
+        }
+        println!("{:?} validity is {valid}", page);
+        println!("new page is {:?}", new_page);
+        if valid {
+            let middle = &page[page.len()/2];
+            println!("the middle number is {middle}");
+            sum_of_middles = sum_of_middles + middle;
+        }
+    }
+
+
+}
+
+fn page_item_breaks_rules(index :usize, num: &i32, page: &Vec<i32>, rules :&Vec<(i32, i32)>) -> usize {
+    for rule in rules.iter().filter(|(a, b)| a == num) {
+        for (slice_index, item) in page[0..index].iter().enumerate() {
+            if item == &rule.1 {
+                return slice_index;
+            }   
+        }
+    }
+    return page.len();
 }
